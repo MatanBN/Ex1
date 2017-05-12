f6d352d2842f6b871098582a0ef1233255daedcd
MatanBenNoach <matanbn22@gmail.com>
2017-05-12 18:57:45 +0300

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Medallion.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MazeGeneratorLib;
+using MazeLib;
+using SearchAlgorithmsLib;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompareSolvers();
+        }
+
+        public static void CompareSolvers()
+        {
+            DFSMazeGenerator mazeGen = new DFSMazeGenerator();
+            Maze maze = mazeGen.Generate(100,100);
+            SearchableMaze sm = new SearchableMaze(maze);
+            Console.WriteLine(maze);
+            BFSSearcher<Position> bfs= new BFSSearcher<Position>();
+            DFSSearcher<Position> dfs = new DFSSearcher<Position>();
+            Solution BFSSolution = bfs.search(sm);
+            Solution DFSSolution = dfs.search(sm);
+            int BFSNodes = bfs.getNumberOfNodesEvaluated();
+            int DFSNodes = dfs.getNumberOfNodesEvaluated();
+            Console.WriteLine($"Nodes: {BFSNodes}");
+            Console.WriteLine($"Nodes: {DFSNodes}");
+            
+
+
+
+
+
+        }
+    }
+}
